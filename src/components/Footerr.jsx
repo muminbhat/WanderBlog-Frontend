a8bf82420e25636d068ccfc959d0ec8d58b10bf5
Mumin Bhat <mumin.fayaz5050@gmail.com>
2023-09-27 10:06:00 +0530

    copyright current year

--- src/components/Footerr.jsx
+++ src/components/Footerr.jsx
@@ -2,27 +2,30 @@ import React from 'react'
 import { Footer } from 'flowbite-react';
 
 const Footerr = () => {
+  const currentYear = new Date().getFullYear();
     return (
       <>
       <hr className="w-90 h-0.5 mx-auto my-4 bg-gray-100 border-0 rounded md:my-2 dark:bg-gray-700" />
         <Footer container className='p-8'>
-          <Footer.Copyright
+          <Footer.Copyright className='mx-5'
             by="WANDERBLOG™"
-            year={2022}
+            year={currentYear}
           />
           <Footer.LinkGroup>
-            <Footer.Link href="#">
+            <Footer.Link className='mx-5' href="/about">
               About
             </Footer.Link>
-            <Footer.Link href="#">
-              Privacy Policy
+            <Footer.Link className='mx-5' href="/maintenance">
+              Privacy & Policy
             </Footer.Link>
-            <Footer.Link href="#">
+            <Footer.Link className='mx-5' href="/maintenance">
               Licensing
             </Footer.Link>
-            <Footer.Link href="#">
+            <a href="mailto:mumin.fayaz05@gmail.com">
+            <Footer.Link className="cursor-pointer">
               Contact
             </Footer.Link>
+          </a>
           </Footer.LinkGroup>
         </Footer>
         </>
